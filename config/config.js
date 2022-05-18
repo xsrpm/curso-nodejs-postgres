428055fff27993a9dead1e527f229f7a743e91b1
César Palma Morante <cemp2703@gmail.com>
2022-05-18 17:18:46 -0500

    limpiando variables de entorno

--- config/config.js
+++ config/config.js
@@ -4,11 +4,7 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   isProd: process.env.NODE_ENV === 'production',
   port: process.env.PORT || 3000,
-  dbUser: process.env.DB_USER || 'cesar',
-  dbPassword: process.env.DB_PASSWORD || 'admin123',
-  dbHost: process.env.DB_HOST || 'localhost',
-  dbName: process.env.DB_NAME || 'my_store',
-  sgbd: process.env.SGBD || 'postgres',
+  sgbd: process.env.DATABASE_URL.substring(0, process.env.DATABASE_URL.indexOf('://')),
   dbUrl: process.env.DATABASE_URL,
 }
 
